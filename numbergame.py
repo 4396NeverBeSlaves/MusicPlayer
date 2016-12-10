d9610c1978a209f493ef539a77e76380c2f67be2
EveR-Xie <1398644012@qq.com>
2016-12-10 21:12:29 +0800

    1.0
    测试

--- numbergame.py
+++ numbergame.py
@@ -1,4 +1,4 @@
-import random #调用random模块
+﻿import random #调用random模块
 n = random.randint (0,100) #生成0-100的随机数字
 i=10 #猜数字的次数
 print("----------猜数字游戏----------")
@@ -20,6 +20,6 @@ while i>1 : #循环条件
     print("还有",i-1,"次机会。")
     guess= int(input("!!!请重新输入一个数字:"))
     i=i-1
-if guess!=n: #当while循环结束并符合判定条件时
+if guess!=n: #当while循环结束并符合判定条件
     print("其实，这个数是",n)
 print('游戏结束')
