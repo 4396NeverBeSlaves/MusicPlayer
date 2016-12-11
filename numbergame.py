44cbd110eb9fe797af566692554f0e1807f7fe4d
EveR-Xie <1398644012@qq.com>
2016-12-11 23:40:26 +0800

    1.2
    增加了判断是否为数字，对代码排版顺序进行调整。

--- numbergame.py
+++ numbergame.py
@@ -1,12 +1,16 @@
 ﻿import random #调用random模块
 n = random.randint (0,100) #生成0-100的随机数字
 i=10 #猜数字的次数
+maxn=100 #最大随机数字
+minn=0 #最小随机数字
 print("----------猜数字游戏----------")
 print("*你共有",i,"次机会*")
-guess = int(input('输入一个数字:')) #输入的数字复制给guess
-maxn=100 #最大随机数字
-minn=0  #最小随机数字
+guess = input("输入一个数字:") #输入的数字复制给guess
+while guess.isdigit()==False: #判为输入是否为数字
+    print("请输入数字!")
+    guess = input("输入一个数字:")
 while i>1 : #循环条件
+    guess=int(guess)
     if guess == n:
         print("猜对了")
         break  #输入数字为产生的随机数时退出循环
@@ -18,8 +22,11 @@ while i>1 : #循环条件
         print("猜错了,你猜的数小于正确数字。")
     print("正确数字数字在",minn,"和",maxn,"之间。") #显示猜数字的区间
     print("还有",i-1,"次机会。")
-    guess= int(input("!!!请重新输入一个数字:"))
-    i=i-1
-if guess!=n: #当while循环结束并符合判定条件时执行
+    i-=1
+    guess= input("!!!请重新输入一个数字:")
+    while guess.isdigit()==False: #判为输入是否为数字
+        print("请输入数字!")
+        guess = input("输入一个数字:")
+if guess!=n and guess.isdigit()==True: #当while循环结束并符合判定条件时
     print("其实，这个数是",n)
 print('游戏结束')
