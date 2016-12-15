8b9e63aeefa1357bea1460c948da32d54c9e8d00
EveR-Xie <1398644012@qq.com>
2016-12-15 23:51:34 +0800

    Version 1.2.2
    添加了一个简单的发送邮件程序。

---
+++ SimpleEmail.py
@@ -0,0 +1,14 @@
+﻿from email.mime.text import MIMEText
+import smtplib
+msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+from_addr = "123@qq.com"
+password = "****" #如果是qq邮箱需要去邮箱设置授权码
+smtp_server = "smtp.qq.com"
+to_addr = "123@qq.com"
+smpt_port="465"
+
+server = smtplib.SMTP_SSL(smtp_server,smpt_port) # SMTP协议默认端口是25
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
